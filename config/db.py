3a80f9a074bc8336ca02ccfde1bc7577985dbb74
Daniel Salin <daniel.salin@gmail.com>
2025-03-25 19:12:55 +0100

    Update db file to initialize with users and paths

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from schemas.schemas import user_schema, path_schema
 
 load_dotenv()
 
@@ -8,4 +9,24 @@ connection_string = f"{os.getenv('MONGODB_URI')}"
 
 client = MongoClient(connection_string)
 
-db = client[os.getenv('MONGODB_DB')]
+db = client[os.getenv("MONGODB_DB")]
+
+
+def create_user_collection():
+    try:
+        db.create_collection("users")
+    except Exception as e:
+        print(e)
+    db.command("collMod", "users", validator=user_schema)
+
+
+def create_path_collection():
+    try:
+        db.create_collection("paths")
+    except Exception as e:
+        print(e)
+    db.command("collMod", "paths", validator=path_schema)
+
+
+create_user_collection()
+create_path_collection()
