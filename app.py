96af29a2a3c4dd65d31bcd2fb29bbdda7980506e
Daniel Salin <daniel.salin@gmail.com>
2025-04-02 21:58:08 +0200

    Update App to make it easier to apply tests via test settings

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ from dotenv import load_dotenv
 from routes.user_route import user_routes
 from routes.path_route import path_routes
 from flask_cors import CORS
+import os
 
+def create_app(test_config=None):
+    """Application factory to create and configure the Flask app."""
+    load_dotenv()
 
-load_dotenv()
+    app = Flask(__name__)
 
-app = Flask(__name__)
-CORS(app, supports_credentials=True)
+    if test_config:
+        app.config.update(test_config)
 
-app.register_blueprint(user_routes, url_prefix="/users")
-app.register_blueprint(path_routes, url_prefix="/paths")
+    CORS(app, supports_credentials=True)
 
+    
+    app.register_blueprint(user_routes, url_prefix="/users")
+    app.register_blueprint(path_routes, url_prefix="/paths")
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
